0c1cf61907fb8e3290f835e4c83febf12abff9c2
Ameer2332 <97825982+Ameer2332@users.noreply.github.com>
2026-07-31 23:44:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+function openStory() {
+
+  const main = document.getElementById("mainContent");
+
+  main.style.display = "block";
+
+  window.scrollTo({
+    top: window.innerHeight,
+    behavior: "smooth"
+  });
+
+  setTimeout(() => {
+    revealElements();
+  }, 500);
+
+}
+
+
+function revealElements() {
+
+  const elements = document.querySelectorAll(".reveal");
+
+  elements.forEach((element) => {
+
+    const position = element.getBoundingClientRect().top;
+
+    if (position < window.innerHeight - 70) {
+      element.classList.add("show");
+    }
+
+  });
+
+}
+
+
+window.addEventListener("scroll", revealElements);
+
+
+window.addEventListener("load", () => {
+
+  setTimeout(() => {
+    revealElements();
+  }, 500);
+
+});
